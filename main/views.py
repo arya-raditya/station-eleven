a5af369dd4889dd1ab437d24ca560904c4b087f0
arya-raditya <aryaraditya.kusuma2006@gmail.com>
2024-09-09 11:11:44 +0700

    fix views.py

--- main/views.py
+++ main/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name' : 'Koka Kola',
-        'price': 5000,
-        'description': 'Soda berkandungan gula tinggi yang siap memaniskan harimu',
-        'stock' : 10
+        'shop name' : 'Station Eleven',
+        'name' : 'Arya Raditya Kusuma',
+        'class' : 'PBP F'
+
+        # 'name' : 'Koka Kola',
+        # 'price': 5000,
+        # 'description': 'Soda berkandungan gula tinggi yang siap memaniskan harimu',
+        # 'stock' : 10
     }
 
     return render(request, "main.html", context)
