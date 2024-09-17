02485eb0d1fa561e932199425c8062ebbb7db517
arya-raditya <aryaraditya.kusuma2006@gmail.com>
2024-09-17 23:45:34 +0700

    add tugas 2 exc readme

--- main/models.py
+++ main/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
     
 class Person(models.Model):
-    name = models.CharField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
     age = models.IntegerField()
     ishappy = models.BooleanField()
